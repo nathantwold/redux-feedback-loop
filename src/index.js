9fc29c07056fe1876df65fadef17aaa31b6d61e4
Nathan Wold <natew79@gmail.com>
2019-09-27 15:33:00 -0500

    components laid out

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const studentReducer = (state = {}, action) => {
+    if (action.type === 'SET_STUDENT_RESPONSE') {
+        console.log(action.payload)
+        return [...state, action.payload]
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        studentReducer
+    }),
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
